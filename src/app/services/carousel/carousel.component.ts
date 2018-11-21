769d188eb648b973ba42841fbcf351d88ae3f0d6
Tim Stoddard <tim.stoddard2@gmail.com>
2018-11-20 20:08:20 -0800

    swag out the slider even more

--- src/app/services/carousel/carousel.component.ts
+++ src/app/services/carousel/carousel.component.ts
@@ -1,39 +1,74 @@
-import { Component, Input } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { animate, style, AnimationBuilder } from '@angular/animations';
+import { Subject, fromEvent } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss'],
-  animations: [
-    trigger('slide', [
-      state('0', style({ transform: 'translate3d(0px,0px,0px)' })),
-      state('1', style({ transform: 'translate3d(-600px,0px,0px)' })),
-      state('2', style({ transform: 'translate3d(-1200px,0px,0px)' })),
-      state('3', style({ transform: 'translate3d(-1800px,0px,0px)' })),
-      transition('* => *', [animate('0.35s')])
-    ])
-  ]
+  styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() items = [];
+  @ViewChild('carousel') carousel;
+  @ViewChild('slideWrapper') slideWrapper;
   currentItemIndex = 0;
+
+  private ngUnsubscribe = new Subject<void>();
+  private slideWidth = 0;
+
+  constructor(private animationBuilder: AnimationBuilder) {}
+
+  ngOnInit() {
+    fromEvent(window, 'resize')
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(() => this.updateSlideWidth());
+  }
+
+  ngAfterViewInit() {
+    this.updateSlideWidth();
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+  }
+
+  doSlide(prevIndex: number, currIndex: number, duration = 250) {
+    const oldX = -1 * prevIndex * this.slideWidth;
+    const newX = -1 * currIndex * this.slideWidth;
+    this.animationBuilder
+      .build([
+        style({ transform: `translate3d(${oldX}px,0px,0px)` }),
+        animate(duration, style({ transform: `translate3d(${newX}px,0px,0px)` }))
+      ])
+      .create(this.slideWrapper.nativeElement)
+      .play();
+  }
 
   next() {
     if (this.currentItemIndex < this.items.length - 1) {
       this.currentItemIndex++;
+      this.doSlide(this.currentItemIndex - 1, this.currentItemIndex);
     }
   }
 
   prev() {
     if (this.currentItemIndex > 0) {
       this.currentItemIndex--;
+      this.doSlide(this.currentItemIndex + 1, this.currentItemIndex);
     }
   }
 
   goToSlide(index: number) {
     if (index >= 0 && index < this.items.length) {
+      const prevIndex = this.currentItemIndex;
       this.currentItemIndex = index;
+      this.doSlide(prevIndex, this.currentItemIndex);
     }
+  }
+
+  private updateSlideWidth() {
+    this.slideWidth = this.carousel.nativeElement.getBoundingClientRect().width;
+    this.doSlide(0, this.currentItemIndex, 0);
   }
 }
