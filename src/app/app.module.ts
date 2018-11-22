996f8936a3f65aac2d7fdba23b134213b6c81de4
Tim Stoddard <tim.stoddard2@gmail.com>
2018-11-22 15:04:58 -0800

    add footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CarouselComponent } from './services/carousel/carousel.component';
 import { DotsComponent } from './services/carousel/dots/dots.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuTriggerComponent } from './header/menu-trigger/menu-trigger.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { MenuTriggerComponent } from './header/menu-trigger/menu-trigger.compone
     CarouselComponent,
     DotsComponent,
     HeaderComponent,
-    MenuTriggerComponent
+    MenuTriggerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
